97d61248f807971c363db214d1265389f3493783
Pranay Vishwakarma <149870314+Pranay0083@users.noreply.github.com>
2024-11-11 14:57:36 +0530

    jwt utills updated

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (user) => {
-  return jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
 };
 
 const verifyToken = (token) => {
